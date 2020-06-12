6858b8d4992157812e174bb144c256f6b846af26
ABHacker Official <63346676+abhackerofficial@users.noreply.github.com>
2020-06-12 21:48:13 +0530

    Update phoneinfoga.sh

--- phoneinfoga.sh
+++ phoneinfoga.sh
@@ -46,14 +46,14 @@ rm -rf PhoneInfoga > /dev/null 2>&1
 rm -rf phoneinfoga > /dev/null 2>&1
 echo
 sleep 2.0
-echo -e '\033[1;92m[''\033[0m*''\033[1;92m]''\033[1;92m PhoneInfoga Installing...'
+echo -e '\033[1;92m[''\033[0m!''\033[1;92m]''\033[1;92m PhoneInfoga Installing...'
 git clone https://github.com/abhackerofficial/PhoneInfoga > /dev/null 2>&1 && cd PhoneInfoga > /dev/null 2>&1 && unzip PhoneInfoga > /dev/null 2>&1 && python3 -m pip install -r requirements.txt > /dev/null 2>&1 && pip2 install colorama > /dev/null 2>&1
 rm -rf PhoneInfoga.zip > /dev/null 2>&1
 rm -rf phoneinfoga.sh > /dev/null 2>&1
 chmod +x *
 sleep 2.0
 echo
-echo -e '\033[1;92m[''\033[0m*''\033[1;92m]''\033[1;92m PhoneInfoga Successfully Installed.'
+echo -e '\033[1;92m[''\033[0m√''\033[1;92m]''\033[1;92m PhoneInfoga Successfully Installed.'
 sleep 2.0
 echo
 printf $'\n\e[1;94m[\e[0m\e[1;77m*\e[0m\e[1;94m] Press Enter To Exit: \e[0m'
